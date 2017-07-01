83fcf20c59c3c31135bb82cf3ad44c26cc46e554
Sam Soffes <sam@soff.es>
2017-07-01 18:19:59 -0500

    Edit keys

--- Contrast/Sources/TextField.swift
+++ Contrast/Sources/TextField.swift
@@ -97,6 +97,51 @@ final class TextField: NSTextField {
 	}
 
 
+	// MARK: - NSResponder
+
+	override func performKeyEquivalent(with event: NSEvent) -> Bool {
+		let commandKey = NSEventModifierFlags.command.rawValue
+		let commandShiftKey = NSEventModifierFlags.command.rawValue | NSEventModifierFlags.shift.rawValue
+
+		if event.type == NSEventType.keyDown {
+			if (event.modifierFlags.rawValue & NSEventModifierFlags.deviceIndependentFlagsMask.rawValue) == commandKey {
+				switch event.charactersIgnoringModifiers! {
+				case "x":
+					if NSApp.sendAction(#selector(NSText.cut(_:)), to: nil, from: self) {
+						return true
+					}
+				case "c":
+					if NSApp.sendAction(#selector(NSText.copy(_:)), to: nil, from: self) {
+						return true
+					}
+				case "v":
+					if NSApp.sendAction(#selector(NSText.paste(_:)), to: nil, from: self) {
+						return true
+					}
+				case "z":
+					if NSApp.sendAction(Selector(("undo:")), to: nil, from:self) {
+						return true
+					}
+				case "a":
+					if NSApp.sendAction(#selector(NSResponder.selectAll(_:)), to: nil, from: self) {
+						return true
+					}
+				default:
+					break
+				}
+			} else if (event.modifierFlags.rawValue & NSEventModifierFlags.deviceIndependentFlagsMask.rawValue) == commandShiftKey {
+				if event.charactersIgnoringModifiers == "Z" {
+					if NSApp.sendAction(Selector(("redo:")), to: nil, from: self) {
+						return true
+					}
+				}
+			}
+		}
+
+		return super.performKeyEquivalent(with: event)
+	}
+
+
 	// MARK: - NSView
 
 	override var intrinsicContentSize: NSSize {
