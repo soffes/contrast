bd6c078bb23c6a00cb5e1956b64f32a217cba1f0
Sam Soffes <sam@soff.es>
2019-11-21 20:14:06 -0800

    Fix canRecordScreen

--- Contrast/Sources/Controllers/EyeDropperController.swift
+++ Contrast/Sources/Controllers/EyeDropperController.swift
@@ -55,21 +55,20 @@ final class EyeDropperController {
 		NSApp.activate(ignoringOtherApps: true)
 
 		// Fallback if no permission
-		// TODO: Re-enable
-//		if #available(macOS 10.15, *), !canRecordScreen() {
-//			let sampler = NSColorSampler()
-//			sampler.show { [weak self] color in
-//				guard let this = self, let color = color else {
-//					return
-//				}
-//
-//				this.colorSampler = nil
-//				NSSound.contrastPickColor.forcePlay()
-//				this.delegate?.eyeDropperController(this, didSelectColor: color, continuePicking: false)
-//			}
-//			colorSampler = sampler
-//			return
-// 		}
+		if #available(macOS 10.15, *), !canRecordScreen() {
+			let sampler = NSColorSampler()
+			sampler.show { [weak self] color in
+				guard let this = self, let color = color else {
+					return
+				}
+
+				this.colorSampler = nil
+				NSSound.contrastPickColor.forcePlay()
+				this.delegate?.eyeDropperController(this, didSelectColor: color, continuePicking: false)
+			}
+			colorSampler = sampler
+			return
+ 		}
 
 		windows = NSScreen.screens.map { screen in
 			let window = EyeDropperWindow(frame: screen.frame)
@@ -109,16 +108,46 @@ final class EyeDropperController {
 
 	@available(macOS 10.15, *)
 	private func canRecordScreen() -> Bool {
+		let runningApplication = NSRunningApplication.current
+		let processIdentifier = runningApplication.processIdentifier
+
 		guard let windows = CGWindowListCopyWindowInfo([.optionOnScreenOnly], kCGNullWindowID)
 			as? [[String: AnyObject]] else
 		{
+			assertionFailure("Invalid window info")
 			return false
 		}
 
-		return windows.allSatisfy { window in
-			let windowName = window[kCGWindowName as String] as? String
-			return windowName != nil
+		for window in windows {
+			// Get information for each window
+			guard let windowProcessIdentifier = (window[String(kCGWindowOwnerPID)] as? Int).flatMap(pid_t.init) else {
+				assertionFailure("Invalid window info")
+				continue
+			}
+
+			// Don't check windows owned by this process
+			if windowProcessIdentifier == processIdentifier {
+				continue
+			}
+
+			// Get process information for each window
+			guard let windowRunningApplication = NSRunningApplication(processIdentifier: windowProcessIdentifier) else {
+				// Ignore processes we don't have access to, such as WindowServer, which manages the windows named
+				// "Menubar" and "Backstop Menubar"
+				continue
+			}
+
+			if window[String(kCGWindowName)] as? String != nil {
+				if windowRunningApplication.executableURL?.lastPathComponent == "Dock" {
+					// Ignore the Dock, which provides the desktop picture
+					continue
+				} else {
+					return true
+				}
+			}
 		}
+
+		return false
 	}
 }
 
