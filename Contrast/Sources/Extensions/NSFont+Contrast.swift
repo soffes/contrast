55b90c5a9fc5b7400237efdccd4eb45177cc3f41
Sam Soffes <sam@soff.es>
2020-06-24 21:35:18 -0700

    Remove custom font

--- Contrast/Sources/Extensions/NSFont+Contrast.swift
+++ Contrast/Sources/Extensions/NSFont+Contrast.swift
@@ -2,6 +2,6 @@ import AppKit
 
 extension NSFont {
 	static func contrastMonoSpace(ofSize size: CGFloat = 13) -> NSFont {
-		NSFont(name: "Native-Regular", size: size)!
+		.monospacedSystemFont(ofSize: size, weight: .regular)
 	}
 }
